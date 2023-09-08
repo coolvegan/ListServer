6480b8b75c18c4b26ccf62df2aba2d52b56fabec
Marco Kittel <marco@kittel.dev>
2023-09-08 20:40:53 +0200

    Implementierung der Nachrichtenhierarchie

---
+++ EssensbestellServer/TextNachricht.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+
+namespace EssensbestellServer
+{
+    public class DtoNachricht
+    {
+        public string data { get; set; }
+        public string aktion { get; set; }
+    }
+
+	public class TextNachricht
+	{
+		public TextNachricht()
+		{
+		}
+		public void ausgabe(Tuple<String, TcpClient> nachricht)
+		{
+            DtoNachricht dtoNachricht = new DtoNachricht();
+            String result = Verstecker.getInstance().gibPublic();
+            result = Convert.ToBase64String(Encoding.ASCII.GetBytes(result));
+            dtoNachricht.aktion = "rsa";
+            dtoNachricht.data = result;
+            string json = JsonSerializer.Serialize(dtoNachricht);
+            TcpClient client = nachricht.Item2;
+            byte[] data = Encoding.ASCII.GetBytes(json);
+            var stream = client.GetStream();
+            stream.Write(data, 0, data.Length);
+        }
+	}
+
+	public class JsonNachricht : TextNachricht
+	{
+        public void ausgabe(Tuple<String, TcpClient> nachricht)
+        {
+			try
+			{
+                JsonDocument json = JsonDocument.Parse(nachricht.Item1);
+                JsonElement wurzel = json.RootElement;
+                Console.WriteLine(nachricht);
+            }
+			catch(Exception e)
+			{
+				base.ausgabe(nachricht);
+			}
+
+        }
+    }
+
+    public class VerschlusselteNachricht : JsonNachricht
+    {
+        public void ausgabe(Tuple<String, TcpClient> nachricht)
+        {
+            try
+            {
+                byte[] b =  Convert.FromBase64String(nachricht.Item1);
+                String msg = Verstecker.getInstance().entschluessel(b);            
+                Console.WriteLine(msg);
+            }
+            catch (Exception e)
+            {
+                base.ausgabe(nachricht);
+            }
+
+        }
+    }
+}
+
---
+++ EssensbestellServer/Verstecker.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace EssensbestellServer
+{
+    public class Verstecker
+    {
+        private static Verstecker verstecker;
+        private string privaterSchluessel;
+        private string publicSchluessel;
+        public String gibPublic()
+        {
+            return publicSchluessel;
+        }
+        private Verstecker(){        
+            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(2048)) // 2048 ist die Schlüssellänge in Bits
+            {
+                // Exportieren des öffentlichen Schlüssels im XML-Format
+                string publicKey = rsa.ExportRSAPublicKeyPem();
+
+                // Exportieren des privaten Schlüssels im XML-Format (nur wenn erforderlich)
+                privaterSchluessel = rsa.ToXmlString(true); // true gibt den privaten Schlüssel zurück
+                publicSchluessel = rsa.ExportRSAPublicKeyPem();
+            }
+        }
+        public static Verstecker getInstance()
+        {
+            if(verstecker == null)
+            {
+                verstecker = new Verstecker();
+                return verstecker;
+            }
+            return verstecker;
+        }
+
+        public String entschluessel(byte[] bytes)
+        {
+            String result = "";
+            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(2048)) // 2048 ist die Schlüssellänge in Bits
+            {
+                // Exportieren des öffentlichen Schlüssels im XML-Format
+                      // Exportieren des privaten Schlüssels im XML-Format (nur wenn erforderlich)
+                rsa.FromXmlString(privaterSchluessel);
+ 
+                try
+                {
+                    byte[] decryptedBytes = rsa.Decrypt(bytes, false);
+                    result = Encoding.UTF8.GetString(decryptedBytes);
+                } catch(CryptographicException e)
+                {
+                    Console.WriteLine("Nachtricht war nicht entschlüsselbar.");
+                }
+                
+
+                return result;
+            }
+        }
+
+		public String verschluessel(String text)
+		{
+            using (RSA rsa = RSA.Create())
+            {
+                byte[] publicKeyBytes = rsa.ExportRSAPublicKey();
+                byte[] encryptedBytes = rsa.Encrypt(Encoding.UTF8.GetBytes(text), RSAEncryptionPadding.OaepSHA256);
+
+                return Convert.ToBase64String(encryptedBytes);
+            }
+        }
+	}
+}
+
